4c57444ba4c066cdef601717026a456620686bc1
Wagner Paulo <wagstalos@hotmail.com>
2024-07-17 10:24:05 -0300

    "Updated Header navigation items and SectionCards component with new icon and card"

--- src/components/section-cards.js
+++ src/components/section-cards.js
@@ -5,16 +5,21 @@ import { TitleSection } from "./title-section";
 import icon1 from "@/assets/icon1.svg";
 import icon2 from "@/assets/icon2.svg";
 import icon3 from "@/assets/icon3.svg";
+import icon4 from "@/assets/icon4.svg";
 
 export function SectionCards() {
   return (
     <section className="pt-40">
-      <ContainerGrid>
-        <TitleSection title="Automatização e Configurações Avançadas" />
-        <p className="max-w-4xl flex mx-auto text-center text-gray-400">
-          Nosso aplicativo permite uma série de configurações que automatizam o
-          processo de agendamento, minimizando a necessidade de intervenção
-          humana. Dentre essas configurações, destacam-se:
+      <div>
+        <TitleSection
+          title="Automatização e Configurações Avançadas"
+          subtitle="Configurações"
+        />
+        <p className="max-w-4xl mx-auto text-center text-gray-400">
+          Nosso aplicativo permite uma série de configurações que{" "}
+          <strong>automatizam o processo de agendamento</strong>, minimizando aS
+          necessidade de intervenção humana. Dentre essas configurações,
+          destacam-se:
         </p>
         <div className="flex flex-col desktop:flex-row px-8 gap-3 mt-20">
           <SectionCardsInner
@@ -36,8 +41,15 @@ export function SectionCards() {
             description="Evite que o mesmo cliente faça múltiplos agendamentos sem querer e
               cause perda de horários."
           />
+          <SectionCardsInner
+            image={icon4}
+            title="Possibilidade do cliente solicitar diversos serviços no mesmo
+          agendamento."
+            description="(Pode parecer básico porém os outros aplicativos não
+          tem.)"
+          />
         </div>
-      </ContainerGrid>
+      </div>
     </section>
   );
 }
